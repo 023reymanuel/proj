bb5eea2359ff754a5120b6a88a03830f62454964
023reymanuel <emumo211@gmail.com>
2024-02-05 08:51:45 +0300

    adding files

---
+++ quizgame.py
@@ -0,0 +1,10 @@
+print("Welcome to my quiz game")
+
+playing = input("Do you wanna play? ")
+if playing !="yes":
+    quit()
+
+print("okay lets play (-_-) ")
+answ = input(" what does CPU stand for? ")
+if answ == "Central Processing Unit":
+    print("correct")
